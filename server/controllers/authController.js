bd6a35c5d7da9f092fa156e751f1b8d0c6d869de
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-10-07 20:03:57 +0530

    Changes in auth middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,32 +7,32 @@ dotenv.config();
 
 export const login = async (req, res) => {
 
-  try {
-    // Find user by username
-    const { username, phone } = req.body;
+    try {
+        // Find user by username
+        const { username, phone } = req.body;
 
-    // Find employee by username and phone
-    const employee = await Employee.findOne({ username, phone });
+        // Find employee by username and phone
+        const employee = await Employee.findOne({ username, phone });
 
-    if (!employee) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+        if (!employee) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        // Generate JWT token with employee ObjectId and isAdmin flag
+        const token = jwt.sign(
+            {
+                id: employee._id,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        // Return token in response
+        res.status(200).json({
+            token,
+            userId: employee._id,
+        });
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error' });
     }
-
-    // Generate JWT token with employee ObjectId and isAdmin flag
-    const token = jwt.sign(
-      {
-        id: employee._id,
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: '1d' }
-    );
-
-    // Return token in response
-    res.status(200).json({
-      token,
-      userId: employee._id,
-    });
-  } catch (error) {
-    return res.status(500).json({ message: 'Server error' });
-  }
 };
