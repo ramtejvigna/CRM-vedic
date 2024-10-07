1b756034b689bfe6adc033767a8d042ee54c96c9
Srikar132 <bunnyking828@gmail.com>
2024-10-07 11:18:21 +0530

    adenployye changes

--- client/src/components/admin-dashboard/Employee/index.jsx
+++ client/src/components/admin-dashboard/Employee/index.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { styled } from '@mui/material/styles';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Avatar, Button, Typography, tableCellClasses } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify"
+import { AiOutlineUserAdd } from "react-icons/ai"
+import { GET_ALL_EMPLOYEES } from '../../../utils/constants';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -38,17 +41,72 @@ const rows = [
 
 export default function Employee() {
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false)
+    const [employees, setEmployees] = useState([]);
+    const recordsPerPage = 5;
+    const [currentPage, setCurrentPage] = useState(1);
+    const lastIndex = currentPage * recordsPerPage;
+    const firstIndex = lastIndex - recordsPerPage;
+    const records = employees.slice(firstIndex, lastIndex);
+    const npages = Math.ceil(employees.length / recordsPerPage);
+    const numbers = [...Array(npages + 1).keys()].slice(1);
+
+    useEffect(() => {
+        const getEmployees = async () => {
+            try {
+                setIsLoading(true);
+                const res = await fetch(`${GET_ALL_EMPLOYEES}`);
+
+                if (!res.ok) {
+                    toast.error("Error !");
+                }
+
+                const data = await res.json();
+                setEmployees(data.employees);
+                setIsLoading(false);
+            } catch (error) {
+                toast.error("Error !");
+            }
+        }
+
+        getEmployees();
+        return () => {
+
+        }
+    }, []);
+
 
     const handleAddEmployeeClick = () => {
         navigate("add-employee");
     };
 
+    const handlePrev = () => {
+        if (currentPage !== 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    const handleNext = () => {
+        if (currentPage !== npages) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+    const handleChangeCPage = (id) => {
+        setCurrentPage(id);
+    }
+
     return (
+        <div className='py-10 h-full'>
+            <div className='bg-white pt-20 flex flex-col  p-5 rounded-xl shadow-lg h-full relative'>
+                <div className='absolute top-[-6%] left-[50%] translate-x-[-50%] xl:w-[95%]  p-5  bg-blue-500 font-semibold text-white rounded-xl flex flex-row justify-between items-center shadow-lg'>
+                    <h1 className='uppercase '>Employee Table</h1>
         <div className='py-10'>
             <div className='bg-white p-5 rounded-xl shadow-lg'>
                 <div className='absolute p-5 top-24 bg-blue-500 font-semibold text-white rounded-xl flex flex-row justify-between shadow-lg w-[160vh]'>
                     <h1>Employee Table</h1>
                     <ul>
+                        <li onClick={handleAddEmployeeClick} className='p-2 flex items-center gap-2 tracking-wider uppercase bg-blue-900 cursor-pointer rounded-lg shadow-xl'> <AiOutlineUserAdd /> Add Employee</li>
                         <li onClick={handleAddEmployeeClick} className='p-2 bg-blue-900 cursor-pointer rounded-lg shadow-xl'>Add Employee</li>
                     </ul>
                 </div>
@@ -91,6 +149,39 @@ export default function Employee() {
                                     {/* Employed Date */}
                                     <TableCell>{row.employedDate}</TableCell>
 
+                                        {/* Edit Action */}
+                                        <TableCell className='space-x-1'>
+                                            <Button onClick={() => navigate(`edit-employee/${employee._id}`)} variant="outlined" size="small" color="primary">
+                                                Edit
+                                            </Button>
+                                            <Button variant="outlined" size="small" color="primary">
+                                                view
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )
+                }
+
+                {/* page nation */}
+                <ul className='flex items-center p-5 gap-5'>
+                    <li onClick={handlePrev} className={`${currentPage == 1 ? "hidden" : " bg-blue-500 text-white"} link cursor-pointer p-2 text-base px-5 rounded-xl`}>
+                        <a href="#"> Prev </a>
+                    </li>
+                    <li className='flex-1 flex overflow-x-scroll scrollbar-hide gap-1'>
+                        {
+                            numbers.map((n, i) => (
+                                <a onClick={() => handleChangeCPage(n)} className={`${currentPage == n ? "bg-blue-500 text-white" : "border-blue-500 border text-blue-500"} text-sm  rounded-full p-2 px-4`} href="#">{n}</a>
+                            ))
+                        }
+                    </li>
+                    <li onClick={handleNext} className={`${currentPage == npages ? "hidden" : " bg-blue-500 text-white"} link cursor-pointer p-2 text-base px-5 rounded-xl `}>
+                        <a href="#"> Next </a>
+                    </li>
+                </ul>
                                     {/* Edit Action */}
                                     <TableCell className='space-x-1'>
                                         <Button onClick={() => navigate(`edit-employee/${row}`)} variant="outlined" size="small" color="primary">
