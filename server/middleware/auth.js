9d8d1224e71ae7ad0b41bc224bd78be2338c292b
Tejassri Avinasha <136706216+avinasha18@users.noreply.github.com>
2024-10-12 09:55:13 +0530

    fixed employee table

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-
+import { Employee } from '../models/User.js';
 export const isAdmin = (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
 
@@ -38,6 +38,30 @@ export const auth = (req, res, next) => {
     res.status(401).json({ message: 'Token is not valid' });
   }
 };
+
+
+export const tokenExpirationMiddleware = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Update isOnline status to true if the token is valid
+        await Employee.findOneAndUpdate({ _id: decoded.id }, { isOnline: true });
+
+        next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            // Update isOnline status to false if the token has expired
+            const decoded = jwt.decode(token);
+            await Employee.findOneAndUpdate({ _id: decoded.id }, { isOnline: false });
+
+            return res.status(401).json({ message: 'Token expired' });
+        }
+
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
 
 // export const isAdmin = (req, res, next) => {
 //   if (!req.user.isAdmin) {
