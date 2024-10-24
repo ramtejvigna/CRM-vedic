65b0b242ddddc29e6c78be4419bdba81fb116d9d
Tejassri Avinasha <136706216+avinasha18@users.noreply.github.com>
2024-10-24 19:46:08 +0530

    fixed comments

--- client/src/components/admin-dashboard/TaskManagement/index.jsx
+++ client/src/components/admin-dashboard/TaskManagement/index.jsx
@@ -160,9 +160,9 @@ const TaskManagement = () => {
   const handleAddComment = async (id) => {
     setIsAddingComment(true);
     try {
-      await axios.post(`https://vedic-backend-neon.vercel.app/api/employee/tasks/${id}/comments`, {
+      await axios.post(`https://vedic-backend-neon.vercel.app/api/admin/tasks/${id}/comments`, {
         text: newComment,
-        createdBy: "Admin", // Example user ID
+        createdBy: "Admin",
       });
       setSelectedTask((prevTask) => ({
         ...prevTask,
