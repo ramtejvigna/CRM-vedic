ac185313d3a6fbd4850779bea048ae5cdf2c5721
Tejassri Avinasha <136706216+avinasha18@users.noreply.github.com>
2024-10-26 22:12:06 +0530

    fixed comments

--- client/src/components/admin-dashboard/Leaves/index.jsx
+++ client/src/components/admin-dashboard/Leaves/index.jsx
@@ -66,6 +66,7 @@ const Leaves = () => {
 
   const handleFilterChange = (e) => {
     const { name, value } = e.target;
+    console.log(name)
     setFilters({
       ...filters,
       [name]: value,
@@ -263,7 +264,7 @@ const Leaves = () => {
                 </div>
               </div>
 
-              <div className="space-y-2">
+              {/* <div className="space-y-2">
                 <label className="block text-sm font-medium text-gray-700">
                   Search Employee
                 </label>
@@ -278,7 +279,7 @@ const Leaves = () => {
                     className="w-full p-2 pl-10 border rounded-lg focus:ring-2 focus:ring-blue-500"
                   />
                 </div>
-              </div>
+              </div> */}
             </div>
           </motion.div>
         )}
