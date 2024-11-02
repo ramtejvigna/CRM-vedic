9ebca9f026cc4ad8c9fc4e2cc3dbb081554f410c
Srikar132 <bunnyking828@gmail.com>
2024-11-02 20:34:26 +0530

    removed uploads

--- server/server.js
+++ server/server.js
@@ -32,9 +32,8 @@ const __dirname = dirname(__filename);
 app.use(cors());
 app.use(express.json({limit : '50mb'}));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-// 
-///
+
+
 app.use(express.json());
 app.use(bodyParser.json());
 
