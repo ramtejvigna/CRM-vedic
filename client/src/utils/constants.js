3cb2be071ce14e931f1642ae4943e6cc588470ea
Srikar132 <bunnyking828@gmail.com>
2024-12-24 21:23:46 +0530

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST = "http://localhost:9000"
+export const HOST = "https://vedic-backend-neon.vercel.app"
 
 export const EMPLOYEE_ROUTE = `${HOST}/api/employees`;
 export const ADD_EMPLOYEE_ROUTE = `http://localhost:9000/api/employees/add-employee`;
@@ -8,4 +8,4 @@ export const UPDATE_EMPLOYEE = `${EMPLOYEE_ROUTE}/update-employee`
 
 export const ADD_SALARY_STATEMENT = `${HOST}/salaries/`
 export const GET_ALL_SALARIES = `${HOST}/salaries/`
-export const api = "http://localhost:9000"
+export const api = "https://vedic-backend-neon.vercel.app"
